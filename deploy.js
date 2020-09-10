f4b074dfa9f3685424114f694b9271bc02d69279
ken <ken@tutoring.co.kr>
2020-09-10 17:54:07 +0900

    deploy 스크립트 개발

---
+++ deploy.js
@@ -0,0 +1,55 @@
+const {config} = require('./deploy.config.js')
+const { exec } = require("child_process")
+const args = process.argv.slice(2)
+const env = args[0]
+const proj = config.projectId
+const user = config.developer
+const testHost = config.testHost
+const realHost = config.realHost
+
+
+run();
+function run() {
+	deploy().catch(errMsg => {
+		console.log(errMsg);
+	});
+}
+
+
+
+async function deploy() {
+	await upload();
+	console.log(`\n>>>>> ${isRelease()?'릴리즈':env+' 배포'} 성공 ~ ~ ~ <<<<<\n`);
+}
+
+
+function upload() {
+	const cmd =	`rsync -auz --delete ./dist/ ${user}@${getHost()}:/www/${env}/b2b/${proj}/`;
+
+	return new Promise((resolve, reject) => {
+		console.log(`\n\n>>> 업로드 시작`);
+		exec(cmd, (error, stdout, stderr) => {
+			if(error) reject(`>>> 업로드 에러: ${error} : ${stderr}`);
+
+			const lines = stdout.split("\n");
+			lines.map(line => {
+				if(line.startsWith('Uploading')) console.log(line);
+			});
+
+			console.log(`>>> 업로드 끝`);
+
+			resolve();
+		});
+	});
+}
+
+
+
+
+
+function isRelease() {
+	return env == 'REAL';
+}
+function getHost() {
+	return isRelease() ? realHost : testHost;
+}
