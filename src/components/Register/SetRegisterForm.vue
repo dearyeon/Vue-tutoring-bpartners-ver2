245631f0957f0d75abbc316be7e686e035ed7ee0
chelseytutoring <chelsey.a@tutoring.co.kr>
2020-09-22 12:12:20 +0900

    신청 양식 설정 페이지 입력 폼 수정

--- src/components/Register/SetRegisterForm.vue
+++ src/components/Register/SetRegisterForm.vue
@@ -3,11 +3,9 @@
     <div class="col-lg-12">
       <div class="ibox-title title">
         <h2 class="pull-left">신청 양식 설정</h2>
-        <router-link :to="{ path: '/createPage' }">
-          <button class="btn btn-blue-line pull-right">
-            뒤로가기
-          </button>
-        </router-link>
+        <button class="btn btn-blue-line pull-right" @click="$router.go(-1)">
+          뒤로가기
+        </button>
       </div>
     </div>
     <div class="row">
@@ -28,14 +26,11 @@
                 <tr>
                   <th>CI/BI 등록</th>
                   <td>
-                    <!-- TODO: image uploader -->
-                    <!--<tr class="text-center">-->
                     <tr>
                       <td colspan="2">
                         <img :src="preview" alt="업로드 된 이미지" />
                       </td>
                     </tr>
-                    <!--</tr>-->
                     <tr>
                       <td class="col-lg-6">
                         <label class="btn btn-success col-lg-12" for="file">이미지 변경</label>
@@ -52,43 +47,56 @@
             <div class="col-lg-6">
               <table class="table">
                 <tr>
-                  <th>수강신청 문의</th>
+                  <th style="vertical-align: top; padding-top: 12px;">수강신청 문의</th>
                   <td>
-                    <textarea name="info" rows="10" readonly>
-                        고객센터: 1522-7802(평일 10시 ~ 18시)&#10;이메일 문의: b2b@tutoring.co.kr
-                    </textarea>
+                    <div id="text-area">
+                      <ul>
+                        <li>
+                          고객센터: 1522-7802(평일 10시 ~ 18시)
+                        </li>
+                        <li>
+                          이메일 문의: b2b@tutoring.co.kr
+                        </li>
+                      </ul>
+                    </div>
                   </td>
                 </tr>
               </table>
             </div>
           </div>
           <h3 id="step-title">2 step. 신청시 주의 사항(수강 신청 브릿지 화면)</h3>
-          <div class="row">
-            <div class="col-lg-2">
-              <strong>제목</strong>
-            </div>
-            <div class="col-lg-10"><input type="text" class="form-control" placeholder="제목을 입력해 주세요." /></div>
-            <div class="col-lg-2">
-              <strong>내용</strong>
-            </div>
-            <div class="col-lg-10">
-              <p>dj</p>
-              <!-- TODO: text editor pane -->
-            </div>
-            <div class="col-lg-2">
-              <label class="label-w-checkbox" for="agree">
-                동의 항목
-              </label>
-              <input type="checkbox" id="agree" />
-            </div>
-            <div class="col-lg-10">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="내용을 입력해 주세요. 예) 위 사전 유의사항을 확인하였습니다."
-              />
-            </div>
-          </div>
+          <table class="table">
+            <tr>
+              <th class="col-lg-2">
+                <strong class="label-w-checkbox">제목</strong>
+              </th>
+              <td class="col-lg-10"><input type="text" class="form-control" placeholder="제목을 입력해 주세요." /></td>
+            </tr>
+            <tr>
+              <th class="col-lg-2">
+                <strong class="label-w-checkbox">내용</strong>
+              </th>
+              <td class="col-lg-10">
+                <textarea id="disclaimer" class="form-control" v-model="disclaimer"></textarea>
+              </td>
+            </tr>
+            <tr>
+              <th class="col-lg-2">
+                <label class="label-w-checkbox" for="agree">
+                  동의 항목
+                </label>
+                <input type="checkbox" id="agree" />
+              </th>
+              <td class="col-lg-10">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="내용을 입력해 주세요. 예) 위 사전 유의사항을 확인하였습니다."
+                />
+              </td>
+            </tr>
+          </table>
+
           <h3 id="step-title">3 step. 개인정보 수집</h3>
           <table class="table">
             <thead>
@@ -161,9 +169,7 @@
           <h3 id="step-title">4 step. 결제시 유의사항</h3>
           <div class="row">
             <div class="col-lg-2">
-              <strong>
-                유의사항
-              </strong>
+              <strong class="label-w-checkbox">유의사항</strong>
             </div>
             <div class="col-lg-10">
               <table class="table">
@@ -171,12 +177,14 @@
                   <input type="text" class="form-control" value="[결제 유의사항]" />
                 </tr>
                 <br />
-                <textarea rows="5" readonly>
-                        입력하신 정보로 정기 결제가 진행됩니다.
-                        미출석시 다음달 1일 수강료가 과금될 수 있으니 주의해주세요.
-                        학습 종료 후 수료 기준에 따라 차액이 자동 결제 됩니다.
-                        수강기간 종료 시 카드 정보는 폐기합니다.
-                </textarea>
+                <div id="text-area">
+                  <ul>
+                    <li>입력하신 정보로 정기 결제가 진행됩니다.</li>
+                    <li>미출석시 다음달 1일 수강료가 과금될 수 있으니 주의해주세요.</li>
+                    <li>학습 종료 후 수료 기준에 따라 차액이 자동 결제 됩니다.</li>
+                    <li>수강기간 종료 시 카드 정보는 폐기합니다.</li>
+                  </ul>
+                </div>
               </table>
             </div>
           </div>
@@ -197,6 +205,8 @@ export default {
       image: "",
       preview:
         "https://s3-alpha-sig.figma.com/img/911a/6151/4c7eb8a41cad075d0c95050d0bf9576a?Expires=1601251200&Signature=gGMNkI8zHURCtidWQDFKLvJGqsKcUdIrZUd3JdBrGzBxWndZmZXAoJpK93nZ37edqjSwuD9EG3F18NctLiN7rX-w0oby8mJquoOA8SMJXieOZbVFR7O~Xn3S~N0Ud8a5IX3YXqHrs9Qky3xrPyqXYDW-ro8WpRZaXIC1dE9Ico9Y6fG45PzOUAkhggVNYGhl8YX121Oup9bsGyvGfonKlFRUNtnz4X9cjwe4OjyYh0K0vfq4P40Q0Qklmxg-~uOjNGRiyMW5w9J6SM-LIchcxaXHoxY43qqFgYl5StkwVSTp1UmX-Fv8CNgFyGnJPYA-cKwfLjsH-vqZWV~HyABA3Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+      disclaimer:
+        "*신청시 주의 사항을 확인해주세요\n*프로그램은 인당 월 1개에 한해 가능합니다.\n(중복 신청시 마지막 신청 정보로 반영)\n*신청 및 정정은 기한내에만 가능합니다.\n(신청~25일까지, 정정 및 취소 26~말일까지)\n*신청 기한내(~25일) 정정은 재 신청 해주시고, 신청 기한 이후(26일~말일) 정정 및 취소는 하기 메일로 요청 해주시기 바랍니다.(b2b@tutoring.co.kr)\n*신청자에 한해 27일 학습 안내 메일이 발송될 예정입니다.\n*GTP 대상/비대상에 따라 급여공제가 진행되므로 GTP여부를 반드시 확인 후 신청 해주시기 바랍니다.(비용 익월 공제)",
     };
   },
   watch: {
@@ -228,7 +238,6 @@ export default {
 .table td,
 .table tbody tr td {
   padding-top: 6px;
-  vertical-align: top;
   border: none;
 }
 .btn-blue-line {
@@ -237,11 +246,14 @@ export default {
   border: 1px solid #1e9ed3;
   border-radius: 0px;
 }
-textarea {
-  resize: none;
+#text-area {
   width: 100%;
+  height: 150px;
   border: solid 1px #e5e6e7;
-  padding: 8px;
+  padding-top: 20px;
+}
+#text-area li {
+  line-height: 24px;
 }
 #step-title {
   margin: 40px auto 20px;
@@ -271,5 +283,8 @@ img {
   height: 240px;
   object-fit: contain;
   background-color: rgba(255, 0, 0, 0.06);
+}
+#disclaimer {
+  height: 300px;
 }
 </style>
