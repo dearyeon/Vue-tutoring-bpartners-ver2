5e6b12e404d98396b077ac3a2241982fb44515db
ken <ken@tutoring.co.kr>
2020-09-24 16:59:39 +0900

    ST 경우, st용 api host 적용

--- src/common/api.js
+++ src/common/api.js
@@ -3,11 +3,14 @@ import querystring from "querystring";
 import shared from "@/common/shared";
 import {config} from "../../deploy.config"
 
-let server = window.location.hostname.startsWith('partners') ? 'https://bapi.tutoring.co.kr' : 'https://bapi-dev.tutoring.co.kr';
-
+const hostId = window.location.hostname.split('.')[0];
+let server = 'https://bapi.tutoring.co.kr';
+if(hostId=='partners-st') {
+    server = 'https://bapi-st.tutoring.co.kr';
+}
 //로컬 API 개발자 전용 설정
-if(config.developer=='ken' && window.location.hostname=='localhost') {
-     server = 'https://localhost';
+else if(config.developer=='ken' && window.location.hostname=='localhost') {
+    server = 'https://localhost';
 }
 
 const api = {
