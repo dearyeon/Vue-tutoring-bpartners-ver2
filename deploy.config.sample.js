d1934f34117b165c84c6f58826cb77106268c01d
ken <kenfmc@gmail.com>
2020-09-27 19:17:46 +0900

    deploy.config.sample.js 사용법 추가

--- deploy.config.sample.js
+++ deploy.config.sample.js
@@ -1,6 +1,7 @@
+//현재 경로에 deploy.config.js로 복사한 후, developer에 각자 본인 ID 기입 하시오
 exports.config = {
   "projectId" : "PARTNERS",
-  "developer" : "ken",
+  "developer" : "_input_your_id_here_",
   "realHost" : "partners2.tutoring.co.kr",
   "testHost" : "partners-st.tutoring.co.kr"
 }
