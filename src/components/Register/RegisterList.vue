e4fbe95896fccbf80b656263e0acc535cfa2d343
endryututoring <endryu@tutoring.co.kr>
2020-10-15 12:57:31 +0900

    by endryu RegisterList.vue 소스 레팩토링

--- src/components/Register/RegisterList.vue
+++ src/components/Register/RegisterList.vue
@@ -1,37 +1,37 @@
 <template>
-  <div class="row">
-    <div class="col-lg-12">
-      <div class="ibox-title title">
-        <h2 class="pull-left">신청페이지 관리</h2>
-        <router-link :to="{ path: '/register/createPage' }">
-          <button class="btn btn-success pull-right">
-            페이지 생성하기
-          </button>
-        </router-link>
-      </div>
-    </div>
-    <div class="row">
-      <div class="ibox content">
-        <div class="ibox-content">
-          <table class="table text-center">
-            <thead>
-              <tr>
-                <th class="text-center">No</th>
-                <th class="text-center">고객사</th>
-                <th class="text-center">담당자</th>
-                <th class="text-center">신청차수</th>
-                <th class="text-center">신청양식설정</th>
-                <th class="text-center">신청양식수정일자</th>
-                <th class="text-center">URL</th>
-              </tr>
-            </thead>
-            <tr v-for="(item, index) in list" :key="`Register-${index}`">
-              <td>{{ item.no }}</td>
-              <td>
-                {{ item.site.company }}
-              </td>
-              <td>{{ item.site.name }}</td>
-              <td>
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="ibox-title title">
+				<h2 class="pull-left">신청페이지 관리</h2>
+				<router-link :to="{ path: '/register/createPage' }">
+					<button class="btn btn-success pull-right">
+						페이지 생성하기
+					</button>
+				</router-link>
+			</div>
+		</div>
+		<div class="row">
+			<div class="ibox content">
+				<div class="ibox-content">
+					<table class="table text-center">
+						<thead>
+						<tr>
+							<th class="text-center">No</th>
+							<th class="text-center">고객사</th>
+							<th class="text-center">담당자</th>
+							<th class="text-center">신청차수</th>
+							<th class="text-center">신청양식설정</th>
+							<th class="text-center">신청양식수정일자</th>
+							<th class="text-center">URL</th>
+						</tr>
+						</thead>
+						<tr v-for="(item, index) in list" :key="`Register-${index}`">
+							<td>{{ item.no }}</td>
+							<td>
+								{{ item.site.company }}
+							</td>
+							<td>{{ item.site.name }}</td>
+							<td>
 								<select v-model="item.selectedApplyIdx">
 									<option v-for="(apply,i) in item.applys" :value="i">
 										{{apply.a_no}}회차 | {{ moment(apply.apply_to_dt).format('YYYY-MM-DD') }} - {{moment(apply.apply_fr_dt).format('YYYY-MM-DD') }}
@@ -49,7 +49,7 @@
 								</div>
 							</td>
 							<td>
-								<button class="btn btn-primary" @click="goToApplyPage(item.applyAdminUrl)">페이지 보러 가기</button>
+								<button class="btn btn-primary" @click="goToApplyPage('https://apply.tutoring.co.kr/'+item.applys[item.selectedApplyIdx].hash+'/7788')">페이지 보러 가기</button>
 							</td>
 						</tr>
 					</table>
@@ -78,8 +78,7 @@
 			const res = await api.get('/partners/applyPageList')
 			const list = res;
 			list.forEach(item => {
-				item.selectedApplyIdx = 0,
-				item.applyAdminUrl = 'https://apply.tutoring.co.kr/'+item.applys[item.selectedApplyIdx].hash+'/7788'
+				item.selectedApplyIdx = 0
 			})
 			this.list = list
 		},
