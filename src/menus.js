778026803ca989794af9763a29986f61c2a28305
endryututoring <endryu@tutoring.co.kr>
2020-10-26 10:22:50 +0900

    by endryu

--- src/menus.js
+++ src/menus.js
@@ -1,10 +1,10 @@
 const menus = [
-    {name:'수업 현황', id:'lesson', path:'/lesson/List', icon:'pie-chart'},
+    {name:'수강 현황', id:'lesson', path:'/lesson/List', icon:'pie-chart'},
     // {name:'학생 현황', id:'user', path:'/user/List', icon:'list'},
     {name:'신청/입과 관리', id:'apply', path:'/apply/List', icon:'star'},
     {name:'결제 관리', id:'billing', path:'/billing/List', icon:'id-badge'},
     {name:'차수 관리', id:'register', path:'/register/List', icon:'list'},
-    {name:'고객사 관리', id:'customer', path:'/customer/List', icon:'handshake-o'},
+    {name:'사이트 관리', id:'customer', path:'/customer/List', icon:'handshake-o'},
 ]
 
 export default menus;
