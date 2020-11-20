2e64449c3eb878facbd959e1593797b154a01f4b
ken <ken@tutoring.co.kr>
2020-11-20 17:43:58 +0900

    신청리스트-취소일시 추가

--- src/components/Apply/ApplyList.vue
+++ src/components/Apply/ApplyList.vue
@@ -9,7 +9,7 @@
 		<input type="file" id="file" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ref="file" @change="importExcel" />
 
 		<Content>
-			<Table :headers="['No','이름','이메일/고객식별ID','소속','부서','직위','사번'].concat(cfs.map(a => a.title), ['수강권','제공가','회사지원금','자기부담금','접수일시 or 취소일시','취소/복원'])"
+			<Table :headers="['No','이름','이메일/고객식별ID','소속','부서','직위','사번'].concat(cfs.map(a => a.title), ['수강권','제공가','회사지원금','자기부담금','접수일시','취소일시','취소/복원'])"
 				:data="orders"
 					v-slot="{item, i}">
 				<td>{{ i + 1 }}</td>
@@ -24,7 +24,8 @@
 				<td>{{ item.goods ? $shared.nf(item.goods.supply_price) : '' }}</td>
 				<td>{{ item.goods ? $shared.nf(item.goods.supply_price - item.goods.charge_price) : '' }}</td>
 				<td>{{ item.goods ? $shared.nf(item.goods.charge_price) : '' }}</td>
-				<td>{{ item.apply_ccl_dt ? moment(item.apply_ccl_dt).format('YYYY-MM-DD HH:mm') :  moment(item.apply_dt).format('YYYY-MM-DD HH:mm') }}</td>
+				<td>{{ moment(item.apply_dt).format('YYYY-MM-DD HH:mm') }}</td>
+				<td>{{ item.apply_ccl_dt && moment(item.apply_ccl_dt).format('YYYY-MM-DD HH:mm') }}</td>
 				<td v-if="!!item.apply_ccl_dt"><ItemButton text="복원" variant="primary" @click="" /></td>
 				<td v-else><ItemButton text="취소" variant="danger" @click="" /></td>
 			</Table>
