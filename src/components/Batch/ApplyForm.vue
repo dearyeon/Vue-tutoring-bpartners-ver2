f5be1e335344f79e8cf5b23e4d8a091bb09189ec
jerrytutoring <jerry.a@mdcrew.co.kr>
2020-11-30 11:07:35 +0900

    tab정렬

--- src/components/Batch/ApplyForm.vue
+++ src/components/Batch/ApplyForm.vue
@@ -1,168 +1,168 @@
 <template>
-  <div class="row">
-    <div class="col-lg-12">
-      <div class="ibox-title title">
-        <h2 class="pull-left">신청 양식 설정</h2>
-        <button class="btn btn-blue-line pull-right" @click="$router.go(-1)">
-          뒤로가기
-        </button>
-      </div>
-    </div>
-    <div class="row">
-      <div class="ibox content">
-        <div class="ibox-content">
-	        <div class="form-group">
-				<div class="well col-xs-12">
-					<h3 class="col-xs-2 no-margins">1 step. 액세스 홈</h3>
-					<div class="col-xs-3 pull-right">
-						<h3 class="col-xs-6 no-margins">오픈 여부</h3>
-						<div class="col-xs-6">
-							<div class="switch">
-								<div class="onoffswitch">
-									<input class="onoffswitch-checkbox form-control" name="cancel_bach" id="cancel_bach" type="checkbox" v-model="openYn"/>
-									<label class="onoffswitch-label" for="cancel_bach">
-										<span class="onoffswitch-inner"></span>
-										<span class="onoffswitch-switch"></span>
-									</label>
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="ibox-title title">
+				<h2 class="pull-left">신청 양식 설정</h2>
+				<button class="btn btn-blue-line pull-right" @click="$router.go(-1)">
+				뒤로가기
+				</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="ibox content">
+				<div class="ibox-content">
+					<div class="form-group">
+						<div class="well col-xs-12">
+							<h3 class="col-xs-2 no-margins">1 step. 액세스 홈</h3>
+							<div class="col-xs-3 pull-right">
+								<h3 class="col-xs-6 no-margins">오픈 여부</h3>
+								<div class="col-xs-6">
+									<div class="switch">
+										<div class="onoffswitch">
+											<input class="onoffswitch-checkbox form-control" name="cancel_bach" id="cancel_bach" type="checkbox" v-model="openYn"/>
+											<label class="onoffswitch-label" for="cancel_bach">
+												<span class="onoffswitch-inner"></span>
+												<span class="onoffswitch-switch"></span>
+											</label>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>
-	        <div class="row">
-	            <div class="col-lg-6">
-	              <table class="table">
-	                <tr>
-	                  <th>Access code</th>
-	                  <td><input type="text" class="form-control" v-model="accessCode" placeholder="Access code를 입력해 주세요." /></td>
-	                </tr>
-	                <tr>
+						<div class="row">
+							<div class="col-lg-6">
+								<table class="table">
+									<tr>
+										<th>Access code</th>
+										<td><input type="text" class="form-control" v-model="accessCode" placeholder="Access code를 입력해 주세요." /></td>
+									</tr>
+									<tr>
 										<th>이메일 도메인 지정</th>
 										<td><input type="text" class="form-control" placeholder="이메일 도메인을 입력해주세요." v-model="emailDomain"/></td>
 									</tr>
-		              <tr>
-			              <th>제한 인원수</th>
-			              <td><input type="text" class="form-control" placeholder="제한 인원수를 입력해 주세요." v-model="limitCnt"/></td>
-		              </tr>
-	              </table>
-	            </div>
-	            <div class="col-lg-6">
-	              <table class="table">
-	                <tr>
-	                  <th style="vertical-align: top; padding-top: 12px; width:20%">수강신청 문의</th>
-	                  <td>
-	                    <textarea class="form-control" v-model="contacts" style="height:250px"></textarea>
-	                  </td>
-	                </tr>
-					<tr>
-						<th>수강신청기간</th>
-						<td>
-							<date-picker v-model="applyRange" type="datetime"  format="YYYY-MM-DD HH:mm" range placeholder="Select date"></date-picker>
-						</td>
-					</tr>
-	              </table>
-	            </div>
-	          </div>
-	        </div>
-
-	        <div class="hr-line-dashed"></div>
-			<div class="form-group">
-		        <h3 class="well">2 step. 신청시 주의 사항(수강 신청 브릿지 화면)</h3>
-		        <table class="table">
-			        <tr>
-				        <th class="col-lg-2">
-					        <strong class="label-w-checkbox">내용</strong>
-				        </th>
-				        <td class="col-lg-10">
-					        <textarea class="form-control" v-model="notice" style="height:250px"></textarea>
-				        </td>
-			        </tr>
-		        </table>
-			</div>
-
-	        <div class="form-group">
-	        	<div class="hr-line-dashed"></div>
-	          <h3 class="well">3 step. 개인정보 수집</h3>
-	          <div class="drag">
-	            <table class="table table-bordered">
-	              <thead>
-		            <th class="text-center">순서</th>
-	                <th class="text-center">노출여부</th>
-	                <th class="text-center">필수입력</th>
-		            <th class="text-center">column Name</th>
-	                <th class="text-center ">항목</th>
-	                <th class="text-center">질문 내용</th>
-	                <th class="text-center">타입</th>
-		            <th class="text-center">
-			            <button class="btn btn-success" @click="addFormList">row 추가</button>
-		            </th>
-	              </thead>
-		            <draggable :list="applyerFormList" tag="tbody">
-			            <tr v-for="(item,index) in applyerFormList" :key="item.id">
-				            <td class="text-center">
-					            {{sortNumber(item,index)}}
-				            </td>
-				            <td class="text-center">
-					            <input v-model="item.disp_yn" type="checkbox" :checked="item.disp_yn"/>
-				            </td>
-				            <td class="text-center">
-					            <input v-model="item.required" type="checkbox" :checked="item.required"/>
-				            </td>
-				            <td>
-					            <input type="text" class="form-control" v-model="item.col_id" readonly placeholder="column Name을 입력해주세요."/>
-				            </td>
-				            <td>
-					            <input type="text" class="form-control" v-model="item.title" placeholder="항목을 입력해주세요."/>
-				            </td>
-				            <td>
-					            <input type="text" class="form-control" v-model="item.description" placeholder="내용을 입력해주세요."/>
-				            </td>
-				            <td class="text-center">
-					            <div class="col-xs-2">
-						            <select v-model="item.type">
-							            <option value="T">Text</option>
-							            <option value="S">Select</option>
-						            </select>
-					            </div>
-
-					            <div class="col-xs-10" v-if="item.type === 'S'">
-				                <span class="col-xs-6">
-			                    <input type="text" class="form-control" v-model="item.opts" placeholder="option을 '|' 로 구분해서 작성해 주세요."/>
-				                </span>
-						            <span class="col-xs-6">
-				                  <input type="text" class="form-control" v-model="item.vals" placeholder="value를 '|' 로 구분해서 작성해 주세요."/>
-				                </span>
-					            </div>
-				            </td>
-				            <td class="text-center" v-if="item.isCf">
-					            <button class="btn btn-danger" @click="deleteFormList(item.col_id)">삭제</button>
-				            </td>
-			            </tr>
-		            </draggable>
-	            </table>
-	          </div>
-	        </div>
-
-	        <div class="hr-line-dashed"></div>
-			<div class="form-group">
-				<h3 class="well col-xs-12">4 step. 결제정보 입력</h3>
-				<div class="row">
-					<div class="col-lg-2">
-						<strong class="label-w-checkbox">유의사항</strong>
+									<tr>
+										<th>제한 인원수</th>
+										<td><input type="text" class="form-control" placeholder="제한 인원수를 입력해 주세요." v-model="limitCnt"/></td>
+									</tr>
+								</table>
+							</div>
+							<div class="col-lg-6">
+								<table class="table">
+									<tr>
+									<th style="vertical-align: top; padding-top: 12px; width:20%">수강신청 문의</th>
+									<td>
+										<textarea class="form-control" v-model="contacts" style="height:250px"></textarea>
+									</td>
+									</tr>
+									<tr>
+										<th>수강신청기간</th>
+										<td>
+											<date-picker v-model="applyRange" type="datetime"  format="YYYY-MM-DD HH:mm" range placeholder="Select date"></date-picker>
+										</td>
+									</tr>
+								</table>
+							</div>
+						</div>
 					</div>
-					<div class="col-lg-10">
-						<textarea class="form-control" v-model="billNotice"></textarea>
+
+					<div class="hr-line-dashed"></div>
+					<div class="form-group">
+						<h3 class="well">2 step. 신청시 주의 사항(수강 신청 브릿지 화면)</h3>
+						<table class="table">
+							<tr>
+								<th class="col-lg-2">
+									<strong class="label-w-checkbox">내용</strong>
+								</th>
+								<td class="col-lg-10">
+									<textarea class="form-control" v-model="notice" style="height:250px"></textarea>
+								</td>
+							</tr>
+						</table>
+					</div>
+
+					<div class="form-group">
+						<div class="hr-line-dashed"></div>
+						<h3 class="well">3 step. 개인정보 수집</h3>
+						<div class="drag">
+							<table class="table table-bordered">
+							<thead>
+								<th class="text-center">순서</th>
+								<th class="text-center">노출여부</th>
+								<th class="text-center">필수입력</th>
+								<th class="text-center">column Name</th>
+								<th class="text-center ">항목</th>
+								<th class="text-center">질문 내용</th>
+								<th class="text-center">타입</th>
+								<th class="text-center">
+									<button class="btn btn-success" @click="addFormList">row 추가</button>
+								</th>
+							</thead>
+								<draggable :list="applyerFormList" tag="tbody">
+									<tr v-for="(item,index) in applyerFormList" :key="item.id">
+										<td class="text-center">
+											{{sortNumber(item,index)}}
+										</td>
+										<td class="text-center">
+											<input v-model="item.disp_yn" type="checkbox" :checked="item.disp_yn"/>
+										</td>
+										<td class="text-center">
+											<input v-model="item.required" type="checkbox" :checked="item.required"/>
+										</td>
+										<td>
+											<input type="text" class="form-control" v-model="item.col_id" readonly placeholder="column Name을 입력해주세요."/>
+										</td>
+										<td>
+											<input type="text" class="form-control" v-model="item.title" placeholder="항목을 입력해주세요."/>
+										</td>
+										<td>
+											<input type="text" class="form-control" v-model="item.description" placeholder="내용을 입력해주세요."/>
+										</td>
+										<td class="text-center">
+											<div class="col-xs-2">
+												<select v-model="item.type">
+													<option value="T">Text</option>
+													<option value="S">Select</option>
+												</select>
+											</div>
+
+											<div class="col-xs-10" v-if="item.type === 'S'">
+											<span class="col-xs-6">
+											<input type="text" class="form-control" v-model="item.opts" placeholder="option을 '|' 로 구분해서 작성해 주세요."/>
+											</span>
+												<span class="col-xs-6">
+											<input type="text" class="form-control" v-model="item.vals" placeholder="value를 '|' 로 구분해서 작성해 주세요."/>
+											</span>
+											</div>
+										</td>
+										<td class="text-center" v-if="item.isCf">
+											<button class="btn btn-danger" @click="deleteFormList(item.col_id)">삭제</button>
+										</td>
+									</tr>
+								</draggable>
+							</table>
+						</div>
+					</div>
+
+					<div class="hr-line-dashed"></div>
+						<div class="form-group">
+							<h3 class="well col-xs-12">4 step. 결제정보 입력</h3>
+							<div class="row">
+								<div class="col-lg-2">
+									<strong class="label-w-checkbox">유의사항</strong>
+								</div>
+								<div class="col-lg-10">
+									<textarea class="form-control" v-model="billNotice"></textarea>
+								</div>
+							</div>
+						</div>
+
+					<div class="col-xs-4 pull-right p-h-xl">
+						<button class="col-xs-12 btn btn-lg btn-primary" @click="[sendNum(),setForm()]">저장</button>
 					</div>
 				</div>
 			</div>
-
-			<div class="col-xs-4 pull-right p-h-xl">
-				<button class="col-xs-12 btn btn-lg btn-primary" @click="[sendNum(),setForm()]">저장</button>
-			</div>
-        </div>
-      </div>
-    </div>
-  </div>
+		</div>
+	</div>
 </template>
 
 
