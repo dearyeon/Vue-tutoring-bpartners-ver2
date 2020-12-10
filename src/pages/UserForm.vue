69c3aba9cabf9d735768112203e17fcdae63f4f2
jerrytutoring <jerry.a@mdcrew.co.kr>
2020-12-10 11:58:07 +0900

    로그인 폼

--- src/pages/UserForm.vue
+++ src/pages/UserForm.vue
@@ -3,23 +3,21 @@
 	<Header title="본인정보 수정"></Header>
     <div id="content" class="ibox-content">
         <div class="row">
-            <div class="col-md-offset-3 col-lg-4">
-                <table class="table">
-                    <tr>
+            <div class="col-md-offset-3">
+                <div>
+                    <div class="field">
                         <th>현재 비밀번호</th>
                         <td><input type="password" class="form-control" v-model="oldPassword"/></td>
-                    </tr>
-                    <tr>
+                    </div><br/>
+                    <div class="field" style="margin-top:10px">
                         <th>새 비밀번호</th>
                         <td><input type="password" class="form-control" v-model="newPassword"/></td>
-                    </tr>
-                    <tr>
+                    </div><br/>
+                    <div class="field">
                         <th>새 비밀번호 확인</th>
-                        <td>
-                            <input type="password" class="form-control" v-model="newPasswordCheck"/>
-                        </td>
-                    </tr>
-                </table>
+                        <td><input type="password" class="form-control" v-model="newPasswordCheck"/></td>
+                    </div><br/>
+                </div>
             </div>
         </div>
         <div class="col-md-offset-3">
@@ -31,24 +29,46 @@
         </div>
         <div class="hr-line-dashed"></div>
         <div class="row">
-            <div class="col-md-offset-3 col-lg-4">
+            <div class="col-md-offset-3">
+                <div>
+                    <div class="field">
+                        <th>이름</th>
+                        <td><input type="text" class="form-control" v-model="name"/></td>
+                        <span v-if="!name" :style="notice?'color:red':''" style="margin-left:25px;">이름을 입력해 주세요.</span>
+                    </div><br/>
+                    <div class="field">
+                        <th>이메일</th>
+                        <td><input type="text" class="form-control" v-model="email"/></td>
+                        <span v-if="!email" :style="notice?'color:red':''" style="margin-left:25px;">이메일을 입력해 주세요.</span>
+                    </div><br/>
+                    <div class="field">
+                        <th>전화/휴대폰</th>
+                        <td><input type="text" class="form-control" v-model="tel"/></td>
+                        <span v-if="!tel" :style="notice?'color:red':''" style="margin-left:25px;">전화/휴대폰을 입력해 주세요.</span>
+                    </div><br/>
+                </div>
+                <!--
                 <table class="table">
                     <tr>
                         <th>이름</th>
-                        <td><input type="text" class="form-control" v-model="name"/></td>
-                        <span v-if="!name">이름을 입력해 주세요.</span>
+                        <td><input type="text" class="form-control" :placeholder="$shared.getAccount().name" v-model="name"/></td>
                     </tr>
+                    <span v-if="!name" :style="notice?'color:red':''">이름을 입력해 주세요.</span>
+                </table>
+                </div>
+                <table class="table">
                     <tr>
                         <th>이메일</th>
-                        <td><input type="text" class="form-control" v-model="email"/></td>
-                        <span v-if="!email">이메일을 입력해 주세요.</span>
+                        <td><input type="text" class="form-control" :placeholder="$shared.getAccount().email" v-model="email"/></td>
+                        <span v-if="!email" :style="notice?'color:red':''">이메일을 입력해 주세요.</span>
                     </tr>
                     <tr>
                         <th>전화/휴대폰</th>
-                        <td><input type="text" class="form-control" v-model="cel"/></td>
-                        <span v-if="!cel">전화/휴대폰을 입력해 주세요.</span>
+                        <td><input type="text" class="form-control" :placeholder="$shared.getAccount().tel" v-model="tel"/></td>
+                        <span v-if="!tel" :style="notice?'color:red':''">전화/휴대폰을 입력해 주세요.</span>
                     </tr>
                 </table>
+                -->
             </div>
         </div>
         <div class="text-center">
@@ -60,6 +80,7 @@
 
 <script>
 import Header from "@/components/Header.vue";
+import shared from "@/common/shared";
 
 export default {
     data() {
@@ -69,24 +90,26 @@ export default {
             newPasswordCheck: '',
             name: '',
             email: '',
-            cel: '',
+            tel: '',
+            notice: false
         }
     },
     components: {
         Header
     },
+    created() {
+        this.name = this.$shared.getAccount().name
+        this.email = this.$shared.getAccount().email
+        this.tel = this.$shared.getAccount().tel
+    },
     methods: {
         save() {
             const reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{10,}$/;
-            if(this.newPassword !== this.newPasswordCheck) {
+            if(!this.name || !this.email || !this.tel) {
+                this.notice = true
+            } else if(this.newPassword !== this.newPasswordCheck) {
                 this.$swal.fire({
                     title: `새 비밀번호가 서로 일치하지 않습니다.`,
-                    icon: 'warning',
-                    confirmButtonColor: '#ed5565'
-                })
-            } else if (!this.name || !this.email || !this.cel) {
-                this.$swal.fire({
-                    title: `비밀번호는 10자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다.`,
                     icon: 'warning',
                     confirmButtonColor: '#ed5565'
                 })
@@ -129,5 +152,17 @@ export default {
 	line-height: 1.8;
 	padding: 12px 15px;
 	margin: 0px 10px;
+}
+th {
+    width: 140px;
+    padding-top: 6px;
+}
+td {
+    width: 250px;
+    padding-top: 6px;
+}
+.field {
+    display: inline;
+    padding-top: 6px;
 }
 </style>
